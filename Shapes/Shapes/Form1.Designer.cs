72c7a3f458967ec1ffd5f47e5b72915fba77d4fc
Isaac Gardner <81584927+Isaac-J-Gardner@users.noreply.github.com>
2021-03-30 11:43:54 +0100

    Add files via upload

---
+++ Shapes/Shapes/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainmenu = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.circlemaker = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.squaremaker = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.mainmenu.SuspendLayout();
+            this.circlemaker.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.squaremaker.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainmenu
+            // 
+            this.mainmenu.Controls.Add(this.button5);
+            this.mainmenu.Controls.Add(this.button1);
+            this.mainmenu.Controls.Add(this.textBox1);
+            this.mainmenu.Location = new System.Drawing.Point(12, 12);
+            this.mainmenu.Name = "mainmenu";
+            this.mainmenu.Size = new System.Drawing.Size(1000, 500);
+            this.mainmenu.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(460, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 25);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Square";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Circle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(440, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(120, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "SHAPES";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // circlemaker
+            // 
+            this.circlemaker.Controls.Add(this.button4);
+            this.circlemaker.Controls.Add(this.pictureBox1);
+            this.circlemaker.Controls.Add(this.textBox2);
+            this.circlemaker.Controls.Add(this.button3);
+            this.circlemaker.Controls.Add(this.button2);
+            this.circlemaker.Location = new System.Drawing.Point(12, 12);
+            this.circlemaker.Name = "circlemaker";
+            this.circlemaker.Size = new System.Drawing.Size(1000, 500);
+            this.circlemaker.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 20);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(100, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 400);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(479, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(460, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 20);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(520, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 20);
+            this.button2.TabIndex = 0;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // squaremaker
+            // 
+            this.squaremaker.Controls.Add(this.button6);
+            this.squaremaker.Controls.Add(this.pictureBox2);
+            this.squaremaker.Controls.Add(this.textBox3);
+            this.squaremaker.Controls.Add(this.button7);
+            this.squaremaker.Controls.Add(this.button8);
+            this.squaremaker.Location = new System.Drawing.Point(12, 12);
+            this.squaremaker.Name = "squaremaker";
+            this.squaremaker.Size = new System.Drawing.Size(1000, 500);
+            this.squaremaker.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 20);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Back";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(100, 60);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 400);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(479, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(460, 30);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(20, 20);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "<";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(520, 30);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(20, 20);
+            this.button8.TabIndex = 0;
+            this.button8.Text = ">";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 524);
+            this.Controls.Add(this.squaremaker);
+            this.Controls.Add(this.circlemaker);
+            this.Controls.Add(this.mainmenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainmenu.ResumeLayout(false);
+            this.mainmenu.PerformLayout();
+            this.circlemaker.ResumeLayout(false);
+            this.circlemaker.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.squaremaker.ResumeLayout(false);
+            this.squaremaker.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainmenu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel circlemaker;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel squaremaker;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Shapes/Shapes/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainmenu.Visible = true;
+            circlemaker.Visible = false;
+            squaremaker.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mainmenu.Visible = false;
+            circlemaker.Visible = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //creating a bitmap of same width and height as the picture box
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            //checking to see if the text box is in an incorrect format and setting radius
+            int radius = 0;
+
+            try
+            {
+                radius = Convert.ToInt32(textBox2.Text);
+                radius++;
+                textBox2.Text = Convert.ToString(radius);
+            }
+            catch (FormatException)
+            {
+                radius = 0;
+                textBox2.Text = "0";
+            }
+
+            //creating the colour black
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            //drawing the circle by incrementing the angle of theta between 0 and 2 PI
+            for (double theta = 0; theta <= Math.PI * 2; theta += 0.001)
+            {
+                //checking to see if the diametre of the circle is going to go outside the bitmap
+                if (radius * 2 >= height)
+                {
+                    radius = height / 2 - 1;
+                    textBox2.Text = Convert.ToString(radius);
+                }
+
+                //getting the x and y coordinates of the point on the circle
+                double x = radius * Math.Cos(theta);
+                double y = radius * Math.Sin(theta);
+
+                //rounding the result so it can be made a coordinate
+                int ix = Convert.ToInt32(x) + width / 2;
+                int iy = Convert.ToInt32(y) + height / 2;
+                bmp.SetPixel(ix, iy, colour);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //everything the same as above except "--" after radius
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            int radius = 0;
+
+            try
+            {
+                radius = Convert.ToInt32(textBox2.Text);
+                radius--;
+                textBox2.Text = Convert.ToString(radius);
+            }
+            catch (FormatException)
+            {
+                radius = 0;
+                textBox2.Text = "0";
+            }
+
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            for (double theta = 0; theta <= Math.PI * 2; theta += 0.001)
+            {
+                if (radius * 2 >= height)
+                {
+                    radius = height / 2 - 1;
+                    textBox2.Text = Convert.ToString(radius);
+                }
+
+                double x = radius * Math.Cos(theta);
+                double y = radius * Math.Sin(theta);
+                int ix = Convert.ToInt32(x) + width / 2;
+                int iy = Convert.ToInt32(y) + height / 2;
+                bmp.SetPixel(ix, iy, colour);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //everything the same except for checking the users input
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            int radius = 0;
+
+            //checking if the input is in a convertable format
+            try
+            {
+                radius = Convert.ToInt32(textBox2.Text);
+            }
+            catch(FormatException)
+            {
+                radius = 0;
+            }
+
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            for (double theta = 0; theta <= Math.PI * 2; theta += 0.001)
+            {
+                if (radius * 2 >= height)
+                {
+                    radius = height / 2 - 1;
+                    textBox2.Text = Convert.ToString(radius);
+                }
+
+                double x = radius * Math.Cos(theta);
+                double y = radius * Math.Sin(theta);
+                int ix = Convert.ToInt32(x) + width / 2;
+                int iy = Convert.ToInt32(y) + height / 2;
+                bmp.SetPixel(ix, iy, colour);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            circlemaker.Visible = false;
+            mainmenu.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mainmenu.Visible = false;
+            squaremaker.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            squaremaker.Visible = false;
+            mainmenu.Visible = true;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //creating a bitmap of same width and height as the picture box
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            //checking to see if the text box is in the correct format and setting radius
+            int radius = 0;
+
+            try
+            {
+                radius = Convert.ToInt32(textBox3.Text);
+                radius++;
+                textBox3.Text = Convert.ToString(radius);
+            }
+            catch (FormatException)
+            {
+                radius = 0;
+                textBox3.Text = "0";
+            }
+
+            //checking to see if drawing the square would go outside the picture box and cause an error
+            if (radius * 2 >= height)
+            {
+                radius = height / 2 - 1;
+                textBox3.Text = Convert.ToString(radius);
+            }
+
+            //creating the colour
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            //creating 2 lines of length 2 radius above and below the centre at distance radius
+            for (int x = width / 2 - radius; x <= width / 2 + radius; x++)
+            {
+                bmp.SetPixel(x, height / 2 + radius, colour);
+                bmp.SetPixel(x, height / 2 - radius, colour);
+            }
+
+            //creating 2 lines of length 2 radius to the left and right of the centre at distance radius
+            for (int y = height / 2 - radius; y <= height/2 + radius; y++)
+            {
+                bmp.SetPixel(width / 2 + radius, y, colour);
+                bmp.SetPixel(width / 2 - radius, y, colour);
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //everything the same except radius--
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            int radius = 0;
+
+            try
+            {
+                radius = Convert.ToInt32(textBox3.Text);
+                radius--;
+                textBox3.Text = Convert.ToString(radius);
+            }
+            catch (FormatException)
+            {
+                radius = 0;
+                textBox3.Text = "0";
+            }
+
+            if (radius * 2 >= height)
+            {
+                radius = height / 2 - 1;
+                textBox3.Text = Convert.ToString(radius);
+            }
+
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            for (int x = width / 2 - radius; x <= width / 2 + radius; x++)
+            {
+                bmp.SetPixel(x, height / 2 + radius, colour);
+                bmp.SetPixel(x, height / 2 - radius, colour);
+            }
+
+            for (int y = height / 2 - radius; y <= height / 2 + radius; y++)
+            {
+                bmp.SetPixel(width / 2 + radius, y, colour);
+                bmp.SetPixel(width / 2 - radius, y, colour);
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //everything the same except for checking the format
+            int width = 800, height = 400;
+
+            Bitmap bmp = new Bitmap(width, height);
+
+            int radius = 0;
+
+            try
+            {
+                radius = Convert.ToInt32(textBox3.Text);
+            }
+            catch (FormatException)
+            {
+                radius = 0;
+            }
+
+            if (radius * 2 >= height)
+            {
+                radius = height / 2 - 1;
+                textBox3.Text = Convert.ToString(radius);
+            }
+
+            Color colour = new Color();
+            colour = Color.FromArgb(255, 0, 0, 0);
+
+            for (int x = width / 2 - radius; x <= width / 2 + radius; x++)
+            {
+                bmp.SetPixel(x, height / 2 + radius, colour);
+                bmp.SetPixel(x, height / 2 - radius, colour);
+            }
+
+            for (int y = height / 2 - radius; y <= height / 2 + radius; y++)
+            {
+                bmp.SetPixel(width / 2 + radius, y, colour);
+                bmp.SetPixel(width / 2 - radius, y, colour);
+            }
+            pictureBox2.Image = bmp;
+        }
+    }
+}
